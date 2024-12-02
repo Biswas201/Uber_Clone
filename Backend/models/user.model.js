66a521572a3575d452321cd5efe5ca4419202312
anurup <anurupbiswas143@gmail.com>
2024-12-02 22:07:44 +0530

    create models

---
+++ Backend/models/user.model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  fullname: {
+    firstname: {
+      type: String,
+      required: true,
+      minlength: [3, "First name must be at least 3 characters long"],
+    },
+    lasttname: {
+      type: String,
+      minlength: [3, "Last name must be at least 3 characters long"],
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: [5, "Email must bee at least 5 characters"],
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+
+  socketId: {
+    type: String,
+  },
+});
+
+
