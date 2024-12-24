74a5063df43f20fd622928aba16e64b8de1089fe
anurup <anurupbiswas143@gmail.com>
2024-12-24 21:48:18 +0530

    rexicon icons

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
+import "remixicon/fonts/remixicon.css";
 
 const Home = () => {
+  const [panelOpen, setPanelOpen] = useState(false);
+  const panelRef = useRef(null);
+  const panelCloseRef = useRef(null);
   return (
     <div>
-      home
-    </div>
-  )
-}
+      <div className="h-screen relative overflow-hidden">
+        <img
+          className="w-16 absolute left-5 top-5"
+          src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"
+          alt=""
+        />
+        {/* <div className="h-screen w-screen">
 
-export default Home
+        </div> */}
+        <div className="flex flex-col justify-end h-screen absolute top-0 w-full">
+          <div className="h-[30%] p-6 bg-white relative">
+            <h5
+              ref={panelCloseRef}
+              onClick={() => {
+                setPanelOpen(false);
+              }}
+              className="absolute opacity-0 right-6 top-6 text-2xl"
+            >
+              <i className="ri-arrow-down-wide-line"></i>
+            </h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
